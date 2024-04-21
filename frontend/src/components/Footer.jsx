5760643b73ab715aef460a932c1ad3faa7b2f9b1
Imranansari8795 <imranansari87959698@gmail.com>
2024-04-22 02:07:58 +0530

    final commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className='fle item-center justify-center'>
+        <hr className='border border-black'/>
+        <h1 className='text-center py-3 text-sm'>&copy; 2024 Your Company. All rights reserved.</h1>
+    </div>
+    </>
+  )
+}
+
+export default Footer
